6164444926fceba52b976a3588ce0e6801fa98b7
Gabriel Ramirez <gabriel.ramirez@salud.gob.sv>
2023-03-06 14:41:20 -0600

    Números

---
+++ 01-basico/02_datatypes.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Números
+  var a = 10;
+  dynamic b = 15;
+
+  int entero =10;
+  double decimal = 10.5;
+  int? c = null; // ? puede ser null
+
+  print(a);
+  print(b);
+  print(entero+decimal);
+  print(entero+decimal);
+
+}
